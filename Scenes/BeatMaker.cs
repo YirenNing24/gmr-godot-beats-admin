656c293f2da4cea899a2d3a7fdb9c9df919642c5
YirenNing24 <paulanthonyarriola@gmail.com>
2024-07-05 04:24:30 +0800

    updates

--- Scenes/BeatMaker.cs
+++ Scenes/BeatMaker.cs
@@ -5,69 +5,71 @@ namespace Main
 	public partial class BeatMaker : Control
 	{
 		public int LoadPercent = 0;
-		private MenuButton FileMenu;
-		private ScrollContainer WindowScroll;
-		private VBoxContainer TracksContainer;
-		private AudioStreamPlayer AudioStreamPlayer;
-		private SpinBox BPMInput;
-		private SpinBox StartInput;
-		private Control WaveformContainer;
-		private TextureRect WaveFormNode;
+		private MenuButton fileMenu;
+		private ScrollContainer windowScroll;
+		private VBoxContainer tracksContainer;
+		private AudioStreamPlayer audioStreamPlayer;
+		private SpinBox bpmInput;
+		private SpinBox startInput;
+		private Control waveformContainer;
+		private TextureRect waveFormNode;
+		private Control cursorContainer;	
+		private Slider cursorSlider;
+		private Node2D cursorStatic;
+		private Node2D cursorPlayback;
 
 
 		public override void _Ready()
 		{
 			LoadEditor();
-
-
 		}
 
-
-		public void LoadEditor()
+		private void LoadEditor()
 		{
 			LoadNodes();
 			CallDeferred("BuildEditor");
 		}
 
-		public void LoadNodes()
+		private void LoadNodes()
 		{
-		FileMenu = GetNode<MenuButton>("EditorContainer/Panel/HBoxContainer/FileMenu");
-		WindowScroll = GetNode<ScrollContainer>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/WindowScroll");
-		TracksContainer = GetNode<VBoxContainer>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/WindowScroll/VBoxContainer/TracksContainer");
-		AudioStreamPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
-		BPMInput = GetNode<SpinBox>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/Panel/HBoxContainer/BPMInput");
-		StartInput = GetNode<SpinBox>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/Panel/HBoxContainer/StartInput");
-		WaveformContainer = GetNode<Control>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/WindowScroll/VBoxContainer/WaveFormContainer");
-		WaveFormNode = GetNode<TextureRect>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/WindowScroll/VBoxContainer/WaveFormContainer/WaveFormNode");
+			fileMenu = GetNode<MenuButton>("EditorContainer/Panel/HBoxContainer/FileMenu");
+			windowScroll = GetNode<ScrollContainer>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/WindowScroll");
+			tracksContainer = GetNode<VBoxContainer>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/WindowScroll/VBoxContainer/TracksContainer");
+			audioStreamPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+			bpmInput = GetNode<SpinBox>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/Panel/HBoxContainer/BPMInput");
+			startInput = GetNode<SpinBox>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/Panel/HBoxContainer/StartInput");
+			waveformContainer = GetNode<Control>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/WindowScroll/VBoxContainer/WaveFormContainer");
+			waveFormNode = GetNode<TextureRect>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/WindowScroll/VBoxContainer/WaveFormContainer/WaveFormNode");
+			cursorContainer = GetNode<Control>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/WindowScroll/VBoxContainer/WaveFormContainer/CursorContainer");
+			cursorSlider = GetNode<HSlider>("EditorContainer/HBoxContainer2/Panel/VBoxContainer/WindowScroll/VBoxContainer/WaveFormContainer/CursorContainer/CursorSlider");
 		}
 
-		public void BuildEditor()
+		private void BuildEditor()
 		{
 			LoadPercent += 100;
 			FileMenuItems();
 			CallDeferred("SetProcess", true);
 			CallDeferred("SetProcessInput", true);
 		}
+
 		public override void _Process(double delta)
 		{
 		}
 
-		public void FileMenuItems() 
+		private void FileMenuItems() 
 		{
-			var PopUp = FileMenu.GetPopup();
-			PopUp.AddItem("Import Song", 0);
-			PopUp.AddItem("Import Beatmap File", 1);
-			PopUp.AddSeparator();
-			PopUp.AddItem("Open Song From Database", 2);
-			PopUp.AddItem("Open Beatmap File From Database", 3);
-			PopUp.AddSeparator();
-			PopUp.AddItem("Save Beatmap Locally", 4);
-			PopUp.AddItem("Export Beatmap to Database", 5);
-			PopUp.AddSeparator();
-			PopUp.AddItem("Reset", 6);
-			PopUp.AddItem("Exit", 7);
+			var popUp = fileMenu.GetPopup();
+			popUp.AddItem("Import Song", 0);
+			popUp.AddItem("Import Beatmap File", 1);
+			popUp.AddSeparator();
+			popUp.AddItem("Open Song From Database", 2);
+			popUp.AddItem("Open Beatmap File From Database", 3);
+			popUp.AddSeparator();
+			popUp.AddItem("Save Beatmap Locally", 4);
+			popUp.AddItem("Export Beatmap to Database", 5);
+			popUp.AddSeparator();
+			popUp.AddItem("Reset", 6);
+			popUp.AddItem("Exit", 7);
 		}
-
-
 	}
 }
